b8ac57b3538b6b91dc6cb2dd28b74248356f02ee
Sanjay <sanjaysinghmuchhal@gmail.com>
2025-11-14 15:45:22 +0700

    added update, find method related to user, nonce gen and verification WIP

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { DatabaseService } from "src/database/database.service";
-import { Prisma } from '@prisma/client';
-
+import { Prisma, $Enums } from '@prisma/client';
 import { generateNonce } from '@meshsdk/core';
+import { generatePrivateKey, toPublicKey } from "@evolution-sdk/lucid";
+import { Keypair } from '@solana/web3.js';
 
 
 @Injectable()
@@ -16,32 +17,58 @@ export class UsersService {
 
   async create(createUserDto: Prisma.UserWalletCreateInput) {
 
-    // let new_user: Prisma.UserWalletCreateInput = {}
-
     //creates a new entry in db with createUserDto
-    await this.databaseService.userWallet.create({
+    return await this.databaseService.userWallet.create({
       data: createUserDto
-    })
-    return 'This action adds a new user';
+    });
   }
 
     async createOrGetNonce(userAddress: string) {
     // if new user, create new user model in the database
     let userObj = await this.findOne(userAddress);
 
+    const nonce = generateNonce('I agree to the term and conditions of the Mesh: ');
+
     if (!userObj) {
         // create cadano and solana keypair, encrpt it and save it in model
-    }
 
-    const nonce = generateNonce('I agree to the term and conditions of the Mesh: ');
+        // cardano
+        const cardanoPriKey = generatePrivateKey(); // Bech32 encoded private key
+        const cardanoPubKey = toPublicKey(cardanoPriKey); // pubkey
+
+        // solana
+        const keypair = Keypair.generate();
+        const solanaPriKey = keypair.secretKey.toString();
+        const solanaPubKey = keypair.publicKey.toBase58();
+
+        // we should encrypt them here, but for the moment lets skip it
+        // we need to define how we want to encrypt it
+
+        const newUserObj = {
+          walletAddress: userAddress,
+          newCardanoAddress: cardanoPubKey,
+          newCardanoEnPriKey: cardanoPriKey, // NOT ENCRIPTED YET!!!
+          solanaAddress: solanaPubKey,
+          solanaEnPriKey: solanaPriKey, // NOT ENCRIPTED YET!!!
+          status: $Enums.AccountStatus.NOT_VERIFIED,
+          nonce: nonce,
+        }
+
+        return await this.create(newUserObj);
+    }
 
     // do: store 'nonce' in user model in the database // will we take care of this in create method
 
     return nonce;
   }
 
-  async findAll() {
-    return `This action returns all users`;
+  async findAll(status?: 'NOT_VERIFIED' | 'VERIFIED') {
+    if (status) return this.databaseService.userWallet.findMany({
+      where: {
+        status,
+      }
+    })
+    return this.databaseService.userWallet.findMany();
   }
 
   async findOne(userAddress: string) {
@@ -52,8 +79,13 @@ export class UsersService {
     });
   }
 
-  async update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(walletAddress: string, updateUserDto: Prisma.UserWalletUpdateInput) {
+    return this.databaseService.userWallet.update({
+      where: {
+        walletAddress,
+      },
+      data: updateUserDto,
+    });
   }
 
   async remove(id: number) {
